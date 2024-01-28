b09f606d993aa16dfbc7557d83b3a8d4cdd02c71
Dieson <dieson.powikani@gmail.com>
2024-01-28 19:44:31 +0100

    'update'

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hitbox_main/widgets/app_large_text.dart';
 
 // Ein Widget, dass später in der BottomNavigation benutzt wird
 class HomeScreenWidget extends StatelessWidget {
@@ -8,35 +9,38 @@ class HomeScreenWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
             padding: const EdgeInsets.only(
               top: 70,
               left: 20,
             ),
-            child: Row(
-              children: [
-                Icon(
-                  Icons.menu,
-                  size: 30,
-                  color: Colors.black,
+            child: Row(children: [
+              Icon(
+                Icons.menu,
+                size: 30,
+                color: Colors.black,
+              ),
+              Expanded(child: Container()),
+              Container(
+                margin: const EdgeInsets.only(right: 20),
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.grey.withOpacity(0.5),
                 ),
-                Expanded(child: Container()),
-                Container(
-                  margin: const EdgeInsets.only(right: 20),
-                  width: 50,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.grey.withOpacity(0.5),
-                  ),
-                ),
-                SizedBox(
-                  height: 40,
-                ),
-              ],
-            ),
-          )
+              ),
+            ]),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 20),
+            child: AppLargeText(text: 'Popular Workouts'),
+          ),
         ],
       ),
     );
